101a373156990b3915dfb4ba836c89eeeecceb89
Rositsa Nenova <udarensamolet@yahoo.com>
2023-03-15 09:36:40 +0200

    added code and updated README.md

---
+++ Eventmi.Infrastructure/Migrations/20230313234354_AddedPlaceToEvent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eventmi.Infrastructure.Migrations
+{
+    public partial class AddedPlaceToEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Place",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Place",
+                table: "Events");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Eventmi
+
+ASP.NET MVC Project for Workshop 1 from Entity Framework Core Course @ SoftUni.
+
+Implements the Repository-Service Pattern with a hint of Unit of Work Pattern.
+
